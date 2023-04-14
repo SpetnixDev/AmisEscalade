f3435bd6fce4316d76fe57795c5c877831ac9174
SpetnixDev <spetnix@gmail.com>
2023-04-14 17:57:44 +0200

    initial

---
+++ src/main/java/com/escalade/AmisEscalade/AmisEscaladeApplication.java
@@ -0,0 +1,13 @@
+package com.escalade.AmisEscalade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmisEscaladeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmisEscaladeApplication.class, args);
+	}
+
+}
